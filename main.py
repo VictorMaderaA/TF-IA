3766d58d9a389ee09e9faa52e0091a054f75cc64
carmen.godoy <carmen.godoy@alumnos.uneatlantico.es>
2020-12-28 18:00:25 +0100

    primeros avances

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import os
 
 folder = "resources"
+train_path = "resources/train"
+test_path = "resources/test"
 
 
 def load_images_from_folder(folder):
@@ -26,6 +28,36 @@ images = load_images_from_folder(folder)
 resize_images(images)
 
 print("Done")
+
+def rescale_images(image):
+    return cv2.resize(image, (100,100))
+
+def load_data():
+    x_train = []
+    y_train_cat = []
+
+    i = 0
+
+    x = os.scandir(train_path)
+    for directorio in x:
+        for r, d, f in os.walk(directorio):
+            for file in f:
+                x_train.append(prepare_image(os.path.join(r, file)))
+                y_train_cat.append(i)
+        i += 1
+
+def prepare_image(file_path):
+    img = cv2.imread(file_path)
+    # Proceso rescalado y blanco y negro
+    return img
+
+load_data()
+
+
+
+
+
+
 
 
 
