d60a959b17bd57be77bd8bfeeb484f1fe8681a73
carmen.godoy <carmen.godoy@alumnos.uneatlantico.es>
2020-12-29 19:11:56 +0100

    primeros avances

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import cv2
+import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from PIL import ImageFile
+from tensorflow_core.python import confusion_matrix
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
 from network import build_network, store_network
 import matplotlib.pyplot as plt
 
@@ -11,8 +17,25 @@ folder = "resources"
 train_path = "resources/train"
 test_path = "resources/test"
 
+# Mostramos Metricas de una historia
+def show_metrics(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
-# Importar librerías
+# Cargamos Data
 def load_data():
     # Reescalar las imágenes
     train_datagen = ImageDataGenerator(rescale=1. / 255)
@@ -33,35 +56,41 @@ def load_data():
     print("Test Gen")
     print(testing_generator.class_indices)
 
+    return train_generator,testing_generator
+
+
+#Construimos y entrenamos la red
+def train():
     network = build_network()
 
-    # Ajustar el modelo utilizando un generador:
+    train_data,test_data = load_data()
+    # Ajustamos el modelo utilizando un generador:
     history = network.fit_generator(
-        train_generator,
+        train_data,
         epochs=30,
-        validation_data=testing_generator,
+        validation_data=test_data,
         validation_steps=50)
 
     store_network(network)
 
+    show_metrics(history)
 
-    print(history.history.keys())
-    # summarize history for accuracy
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+    predict(network)
+
+# Predecimos sobre una red
+def predict(network):
+    _, (X_test, y_test) = load_data()
+    predicted_classes = network.predict_classes(X_test)
+    correct = np.nonzero(predicted_classes == y_test)[0]
+    incorrect = np.nonzero(predicted_classes != y_test)[0]
+    print("**********MATRIZ DE CONFUSION**********")
+    print(confusion_matrix(y_test, predicted_classes))
+    print(len(correct), "Clasificados correctamente")
+    print(len(incorrect), " Clasificados incorrectamente")
+    print()
+    print("**********REPORTE DE CLASIFICACION**********")
+    print(classification_report(y_test, predicted_classes))
+
 
 
 load_data()
